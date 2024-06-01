0cd94baa5a6c710dc80008892329659b1abb5565
Muhammad Talal <hafiztalalaqeel1@gmail.com>
2024-06-01 23:49:03 +0500

    Updates

--- src/components/Contact/contact.component.jsx
+++ src/components/Contact/contact.component.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
